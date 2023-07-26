195587dfec66efdc74f9782952fded3a6e39800a
lidia <102434840+lidiammp@users.noreply.github.com>
2023-07-25 23:12:34 -0700

    Sound + Flash Length

--- StarsAlign/Assets/Scripts/ButtonSoundPlayer.cs
+++ StarsAlign/Assets/Scripts/ButtonSoundPlayer.cs
@@ -6,6 +6,8 @@ public class ButtonSoundPlayer : MonoBehaviour
     public Button yourButton; // Reference to your button in the scene
     public AudioSource audioSource; // Reference to the AudioSource component
 
+    private bool isPlaying = false;
+
     private void Start()
     {
         // Register a listener for the button's onClick event
@@ -14,11 +16,26 @@ public class ButtonSoundPlayer : MonoBehaviour
 
     private void PlaySoundOnClick()
     {
-        // Play the sound when the button is clicked
-        if (audioSource != null && !audioSource.isPlaying)
+        // If audioSource is not null and is not currently playing, play the sound
+        if (audioSource != null && !isPlaying)
         {
-            audioSource.Play();
+            StartCoroutine(PlaySoundCoroutine());
         }
     }
+
+    private System.Collections.IEnumerator PlaySoundCoroutine()
+    {
+        isPlaying = true;
+        audioSource.Play();
+
+        // Wait until the sound finishes playing
+        yield return new WaitForSeconds(audioSource.clip.length);
+
+        // Reset the isPlaying flag after the sound has finished
+        isPlaying = false;
+    }
 }
+
+
+
 
