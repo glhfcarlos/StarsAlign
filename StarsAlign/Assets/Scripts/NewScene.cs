a14e656ccf071f3923c12713328109e14946a74d
mmartin440 <105955175+mmartin440@users.noreply.github.com>
2023-08-26 00:48:39 -0700

    add xbox controls to the win and loose

--- StarsAlign/Assets/Scripts/NewScene.cs
+++ StarsAlign/Assets/Scripts/NewScene.cs
@@ -7,18 +7,6 @@ public class NewScene : MonoBehaviour
 {
     public string SceneName;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     void OnTriggerEnter2D(Collider2D col)
     {
         if(col.gameObject.GetComponent<PlayerMovement>() != null)
